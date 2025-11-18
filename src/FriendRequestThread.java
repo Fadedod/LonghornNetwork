5a8ebac9a453591e58f1ee1fcf56bb9137fc4331
Fadedod <159088680+Fadedod@users.noreply.github.com>
2025-11-17 22:21:24 -0600

    FriendRequest comments

--- src/FriendRequestThread.java
+++ src/FriendRequestThread.java
@@ -1,10 +1,32 @@
+/**
+ * A Runnable task that represents a friend request happening
+ * in a multithreaded environment.
+ */
 public class FriendRequestThread implements Runnable {
+    
+    private final UniversityStudent sender;
+    private final UniversityStudent receiver;
+
+    /**
+     * Creates a new friend request task.
+     *
+     * @param sender   The student who’s sending the friend request.
+     * @param receiver The student who’s receiving it.
+     */
     public FriendRequestThread(UniversityStudent sender, UniversityStudent receiver) {
-        // Constructor
+        this.sender = sender;
+        this.receiver = receiver;
     }
 
+    /**
+     * What happens when this thread runs.
+     * Both students should add each other as friends using the
+     * thread-safe addFriend method.
+     */
     @Override
     public void run() {
-        // Method signature only
+        // TODO: Use the thread-safe addFriend method here.
+        // sender.addFriend(receiver);
+        // receiver.addFriend(sender);
     }
 }
