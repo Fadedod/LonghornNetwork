ba2fef6992bbf3ae15bfef76f1cfe3bdce1819f9
Fadedod <159088680+Fadedod@users.noreply.github.com>
2025-12-08 11:31:30 -0600

    changes

--- src/PodFormation.java
+++ src/PodFormation.java
@@ -1,28 +1,11 @@
-/**
- * A class for forming pods of students, likely using Prim's algorithm
- * as hinted in the README.
- */
-public class PodFormation {
-    
-    private final StudentGraph graph;
+import java.util.*;
 
-    /**
-     * Constructor for PodFormation.
-     *
-     * @param graph The StudentGraph to use for pod formation.
-     */
+public class PodFormation {
     public PodFormation(StudentGraph graph) {
-        this.graph = graph;
+        // Constructor
     }
 
-    /**
-     * Forms pods of a given size.
-     *
-     * @param podSize The desired size for each pod.
-     */
     public void formPods(int podSize) {
-        // TODO: Implement pod formation logic (e.g., Prim's algorithm).
-        // This will find a Minimum Spanning Tree (MST) or Forest
-        // to group students with the strongest total connections.
+        // Method signature only
     }
 }
