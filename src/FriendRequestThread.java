260d4fa98c4517c4ec1c85930b5b7369963baf13
Fadedod <159088680+Fadedod@users.noreply.github.com>
2025-12-08 11:30:43 -0600

    changes

--- src/FriendRequestThread.java
+++ src/FriendRequestThread.java
@@ -1,32 +1,36 @@
-/**
- * A Runnable task that represents a friend request happening
- * in a multithreaded environment.
- */
-public class FriendRequestThread implements Runnable {
-    
-    private final UniversityStudent sender;
-    private final UniversityStudent receiver;
+import java.util.concurrent.Semaphore;
 
-    /**
-     * Creates a new friend request task.
-     *
-     * @param sender   The student who’s sending the friend request.
-     * @param receiver The student who’s receiving it.
-     */
+public class FriendRequestThread implements Runnable {
+  private UniversityStudent sender;
+
+    private UniversityStudent receiver;
+
+    private static final Semaphore semaphore = new Semaphore(1);
+
+    
+     
     public FriendRequestThread(UniversityStudent sender, UniversityStudent receiver) {
+        // Constructor method
         this.sender = sender;
         this.receiver = receiver;
     }
 
     /**
-     * What happens when this thread runs.
-     * Both students should add each other as friends using the
-     * thread-safe addFriend method.
+     * This method lock and acquire the thread(s) to make multiple and/or concurrent friend
+     * requests.
      */
     @Override
     public void run() {
-        // TODO: Use the thread-safe addFriend method here.
-        // sender.addFriend(receiver);
-        // receiver.addFriend(sender);
+        try {
+            semaphore.acquire();
+            sender.addFriend(receiver.name);
+            receiver.addFriend(sender.name);
+            System.out.println(sender.name + " sent a friend request to " + receiver.name);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.err.println("FriendRequestThread interrupted: " + e.getMessage());
+        } finally {
+            semaphore.release();
+        }
     }
 }
