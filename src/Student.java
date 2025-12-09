50410ec15173bb6f0b10b628ee4bb57b666818db
Fadedod <159088680+Fadedod@users.noreply.github.com>
2025-12-08 19:12:19 -0600

    changes

--- src/Student.java
+++ src/Student.java
@@ -1,19 +1,24 @@
 import java.util.*;
 
-// Abstract class representing a student with various attributes
-// and an abstract method to calculate connection strength with another student.
+
 public abstract class Student {
+    // variables for Student class
     protected String name;
+
     protected int age;
+
     protected String gender;
+
     protected int year;
+
     protected String major;
+
     protected double gpa;
+
     protected List<String> roommatePreferences;
+
     protected List<String> previousInternships;
 
-
-
-    // Find the compatible roommates from a list of students
+    // method to calculate connection strength between students
     public abstract int calculateConnectionStrength(Student other);
 }
